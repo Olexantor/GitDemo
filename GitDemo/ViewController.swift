7785d2fb062989f745e1a736ae3e10830261da57
Alexander Nikolaev <nikolaev.warrior@gmail.com>
2020-11-16 22:58:30 +0500

    Initial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Александр on 16.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
